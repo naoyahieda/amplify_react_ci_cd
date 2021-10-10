7465029ca5d7fbb3135e528f2f95c46d92ef0a62
Naoya Hieda <naoyahieda@gmail.com>
2021-10-10 18:41:41 +0900

    Testとgithub workflow

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/環境変数REACT_APP_NODE_ENVの値は/i);
   expect(linkElement).toBeInTheDocument();
 });
